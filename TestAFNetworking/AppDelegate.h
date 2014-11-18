50f088f20146e11b70f97f894aa9f476db71db61
Adward <adward@lius-Mac-mini.local>
2014-11-18 09:27:31 +0800

    Initial Commit

---
+++ TestAFNetworking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestAFNetworking
+//
+//  Created by Adward on 14/11/18.
+//  Copyright (c) 2014年 iDouKou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestAFNetworking/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestAFNetworking
+//
+//  Created by Adward on 14/11/18.
+//  Copyright (c) 2014年 iDouKou. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
